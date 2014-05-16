9377aa8e43f4c7b282a5f05415d2f44cbab590dd
Darren Oakley <daz.oakley@gmail.com>
2014-05-16 10:57:36 +0100

    Update the macmillan-utils lib.
    And as a result use the new style guidelines!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,8 @@ Bundler.setup(:default, :test)
 ENV['RACK_ENV'] = 'test'
 
 require 'macmillan/utils/rspec/rspec_defaults'
-require 'macmillan/utils/rspec/codeclimate_helper'
-require 'macmillan/utils/rspec/simplecov_helper'
 require 'macmillan/utils/rspec/webmock_helper'
+require 'macmillan/utils/test_helpers/codeclimate_helper'
+require 'macmillan/utils/test_helpers/simplecov_helper'
 
 require 'bandiera/client'
